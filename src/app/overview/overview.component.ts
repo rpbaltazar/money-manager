1de1aa958223ae7884bf5c7b409dfab1384e1a8e
Rui Baltazar <rui.p.baltazar@gmail.com>
2021-08-05 09:03:34 +0800

    fixed table looks

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -7,16 +7,11 @@ export interface AccountOverview {
 }
 
 const ACCOUNT_INFO: AccountOverview[] = [
-  { name: 'Hydrogen', amount: 1.0079, currency: 'H' },
-  { name: 'Helium', amount: 4.0026, currency: 'He' },
-  { name: 'Lithium', amount: 6.941, currency: 'Li' },
-  { name: 'Beryllium', amount: 9.0122, currency: 'Be' },
-  { name: 'Boron', amount: 10.811, currency: 'B' },
-  { name: 'Carbon', amount: 12.0107, currency: 'C' },
-  { name: 'Nitrogen', amount: 14.0067, currency: 'N' },
-  { name: 'Oxygen', amount: 15.9994, currency: 'O' },
-  { name: 'Fluorine', amount: 18.9984, currency: 'F' },
-  { name: 'Neon', amount: 20.1797, currency: 'Ne' },
+  { name: 'DBS', amount: 50000, currency: 'SGD' },
+  { name: 'Stashaway', amount: 34000, currency: 'SGD' },
+  { name: 'Millennium BCP', amount: 1000, currency: 'EUR' },
+  { name: 'CGD', amount: 12300, currency: 'EUR' },
+  { name: 'Gemini', amount: 90000, currency: 'EUR' }
 ];
 
 @Component({
@@ -26,7 +21,7 @@ const ACCOUNT_INFO: AccountOverview[] = [
 })
 export class OverviewComponent implements OnInit {
   title = 'Overview';
-  displayedColumns: string[] = ['Account Name', 'Amount', 'Currency'];
+  columnsToDisplay: string[] = ['Account Name', 'Amount', 'Currency'];
   dataSource = ACCOUNT_INFO;
 
   constructor() { }
